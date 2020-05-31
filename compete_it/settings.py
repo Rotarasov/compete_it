88ac21a457796c22aaca2faed310fbc2ba73d376
Roman Tarasov <portafortuna12@outlook.com>
2020-05-31 19:29:51 +0300

    Added pagination

--- compete_it/settings.py
+++ compete_it/settings.py
@@ -21,6 +21,8 @@ ALLOWED_HOSTS = []
 AUTH_USER_MODEL = 'users.User'
 
 REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 12,
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
