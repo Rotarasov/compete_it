9fbba15b46b4b372c7de50b6ef0d08fe942ac57e
Roman Tarasov <portafortuna12@outlook.com>
2020-08-24 17:32:13 +0300

    Hid secret key from compete_it/settings.py

--- compete_it/settings.py
+++ compete_it/settings.py
@@ -8,7 +8,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%j%ja3r8w7+-*^8cba=26r%=z*$o)$514noj3t+ef6&+4w(%2k'
+with open('../secret_key.txt') as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
